5a0bba360bf345a0483534c9722f8d5a1fe11d6c
Nayan <dreamerboynayan@gmail.com>
2025-09-11 17:57:25 +0600

    React Practice

---
+++ basic-practice/src/App.jsx
@@ -0,0 +1,64 @@
+
+
+const LoginStatusBtn = (status) =>{
+
+  if(status){
+    return <button>Logout</button>
+  } else{
+    return <button>Login</button>
+  }
+}
+
+const App = () => {
+  let result = 70;
+  let marks = 50;
+
+  let city = ["Dhaka","Delhi", "London", "New Work"]
+  return (
+    
+    <div>
+      <h1 style={{color:"red"}}>Hello</h1>
+      <button onClick={()=> alert("Hello , Good Evening!")}>Submit</button>
+<hr />
+      {marks>80? <h1>Brilliant Student</h1>: <h1>Weak Student</h1>}
+<hr />
+      {(()=>{
+        if(result>=60 && result < 70){
+          return <h1>A-</h1>
+        }
+        else if(result >= 70 && result < 80){
+          return <h1>A</h1>
+        }
+        else if(result >= 80 && result < 90){
+          return <h1>A+</h1>
+        } else{
+          return <h1>Golden A+</h1>
+        }
+      })()}
+<hr />
+      <ol>
+        {
+          city.map((items, i)=>{
+            return <li key={i.toString()}>{items}</li>
+          })
+        }
+      </ol>
+      <hr />
+
+<h1>Login Status</h1>
+{LoginStatusBtn(false)}
+
+
+      
+
+    
+
+
+    
+
+
+    </div>
+  )
+}
+
+export default App
