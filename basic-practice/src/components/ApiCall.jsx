3d89481a2abe8691ddf40f87d1a8ecb264f0cc33
Nayan <dreamerboynayan@gmail.com>
2025-09-16 06:15:13 +0600

    Api fetch data using useEffect

--- basic-practice/src/components/ApiCall.jsx
+++ basic-practice/src/components/ApiCall.jsx
@@ -1,13 +1,39 @@
+// import { useEffect, useState } from "react"
+
 import { useEffect, useState } from "react"
 
 
+// const ApiCall = () => {
+//     const [data, setData] = useState();
+//     useEffect(()=>{
+//         fetch("https://jsonplaceholder.typicode.com/posts")
+//         .then(response => response.json())
+//         .then(json=> setData(json))
+//     })
+//   return (
+//     <div>
+//       {JSON.stringify(data)}
+//     </div>
+//   )
+// }
+
+// export default ApiCall
+
+
+
 const ApiCall = () => {
-    const [data, setData] = useState();
-    useEffect(()=>{
-        fetch("https://jsonplaceholder.typicode.com/posts")
-        .then(response => response.json())
-        .then(json=> setData(json))
-    })
+  const [data, setData] = useState([]);
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+ 
+  useEffect(()=>{
+    (async()=>{
+      let res = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+      let result = await res.json();
+      setData(result)
+
+    })()
+  })
   return (
     <div>
       {JSON.stringify(data)}
